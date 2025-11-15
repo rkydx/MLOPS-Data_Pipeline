95f3cf25d697c9699b25ac5d8e93b68a55bee8e1
Ramakant <ramakant.c@gmail.com>
2025-11-15 23:05:14 +0800

    added the code for exceptoin and logger

---
+++ demo.py
@@ -0,0 +1,28 @@
+# below code is to check the logging config
+# from sourcefiles.logger import logging
+
+# logging.debug("This is a debug message.")
+# logging.info("This is an info message.")
+# logging.warning("This is a warning message.")
+# logging.error("This is an error message.")
+# logging.critical("This is a critical message.")
+
+# --------------------------------------------------------------------------------
+
+# below code is to check the exception config
+from sourcefiles.logger import logging
+from sourcefiles.exception import MyException
+import sys
+
+try:
+    a = 1+'Z'
+except Exception as e:
+    logging.info(e)
+    raise MyException(e, sys) from e
+
+# --------------------------------------------------------------------------------
+
+# from sourcefiles.pipline.training_pipeline import TrainPipeline
+
+# pipline = TrainPipeline()
+# pipline.run_pipeline()
