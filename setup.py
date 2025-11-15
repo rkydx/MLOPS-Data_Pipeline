e58915dfe3d318b57f7c079032e5c30d5ba8d3d5
Ramakant <ramakant.c@gmail.com>
2025-11-15 22:13:52 +0800

    project structure ready, virtual env setup completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sourcefiles",
+    version="0.0.1",
+    author="Ramakant C",
+    author_email="ramakant.c@gmail.com",
+    packages=find_packages()
+)
