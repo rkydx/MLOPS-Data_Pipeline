b9c8faf7ffea772b0c12894bab9352d00d06a306
Ramakant <ramakant.c@gmail.com>
2025-11-17 13:12:44 +0800

    added code for data transformation and updated other associated files

--- sourcefiles/entity/artifact_entity.py
+++ sourcefiles/entity/artifact_entity.py
@@ -10,4 +10,10 @@ class DataIngestionArtifact:
 class DataValidationArtifact:
     validation_status:bool
     message: str
-    validation_report_file_path: str    
+    validation_report_file_path: str
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path:str 
+    transformed_train_file_path:str
+    transformed_test_file_path:str        
